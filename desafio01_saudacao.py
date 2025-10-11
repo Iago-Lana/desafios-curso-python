28d19a48216e9468f89c7569e387868c6f7af8c8
Iago De Lana <iagoferlana@gmail.com>
2025-10-10 21:22:33 -0300

    Adiciona o desafio 01 de saudação.

---
+++ desafio01_saudacao.py
@@ -0,0 +1,6 @@
+""""
+Desafio 01: Crie um Scipt Python que leia o nome de uma pessoa e mostre uma mensangem de boas-vindas de acordo com o valor digitado.
+
+"""
+nome = input('Qual seu nome? ')
+print("Olá",nome, "Prazer em te conhecer!")
