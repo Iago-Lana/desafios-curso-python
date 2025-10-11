8cb6c1fcf86f19546f28347a537237bc3bb6e06e
Iago De Lana <iagoferlana@gmail.com>
2025-10-11 18:59:09 -0300

    Adiciona o desafio 02 de data de nascimento.

---
+++ desafio02_nascimento.py
@@ -0,0 +1,8 @@
+"""
+Desafio 02: Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+
+"""
+dia = input('dia -')
+mes = input('mes -')
+ano = input('ano -')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano, ". Correto?")
