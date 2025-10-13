c4c797d001bf8d4f8c3226d077b0ba522f701a12
Iago De Lana <iagoferlana@gmail.com>
2025-10-13 13:46:23 -0300

    Adiciona o desafio 03 de soma.

---
+++ desafio03_soma.py
@@ -0,0 +1,9 @@
+"""
+Desafio 03: Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+
+"""
+
+n1 = float(input('primeiro numero - '))
+n2 = float (input ('segundo numero - '))
+soma = n1 + n2
+print('A soma é', soma)
