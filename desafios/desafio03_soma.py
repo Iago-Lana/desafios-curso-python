580625d1c1c306ea0cfe71cc95428fa1dea0eff6
Iago De Lana <iagoferlana@gmail.com>
2025-10-13 16:44:37 -0300

    Modificação no desafio 03 de soma.

--- desafios/desafio03_soma.py
+++ desafios/desafio03_soma.py
@@ -3,7 +3,7 @@ Desafio 03: Crie um script Python que leia dois números e tente mostrar a soma 
 
 """
 
-n1 = float(input('primeiro numero - '))
-n2 = float (input ('segundo numero - '))
+n1 = int(input('primeiro numero - '))
+n2 = int(input ('segundo numero - '))
 soma = n1 + n2
-print('A soma é', soma)
+print('A soma entre {} e {} vale {}'.format(n1,n2,soma))
