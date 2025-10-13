f32c00624fd2aabc6c05e52ef07b39b48a551090
Iago De Lana <iagoferlana@gmail.com>
2025-10-13 15:28:02 -0300

    Adiciona o exercício 002 de saudação.
    Como o exercício 002 é idêntico ao desafio 01 optei por não fazê-lo, para evitar repetição.

---
+++ exercicos/ex002_respondendo_ao_usuario.py
@@ -0,0 +1,7 @@
+"""
+Exercício 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+
+Nota: a solução para esse exercício é a mesma presente no arquivo: aulas/desafio01_saudacao.py
+
+"""
