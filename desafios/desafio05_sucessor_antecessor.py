b606f6a317cc04b4aae134ef5407f69248c0ef1b
Iago De Lana <iagoferlana@gmail.com>
2025-10-17 20:11:22 -0300

    Alteração no nome de algumas variáveis.
    Mudança no nome da variáveis para melhor organização e clareza.

--- desafios/desafio05_sucessor_antecessor.py
+++ desafios/desafio05_sucessor_antecessor.py
@@ -2,5 +2,5 @@
 Desafio 05: Faça um progrma que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
 
 """
-n1 = int(input('Digite um número inteiro: '))
-print('O sucessor de {} é {} e o antecessor é {}'.format(n1, n1+1, n1-1))
+n = int(input('Digite um número inteiro: '))
+print('O sucessor de {} é {} e o antecessor é {}'.format(n, n+1, n-1))
--- desafios/desafio06_dobro_triplo_raiz.py
+++ desafios/desafio06_dobro_triplo_raiz.py
@@ -2,5 +2,5 @@
 Desafio 06: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 
 """
-n1 = int(input('Digite um número: '))
-print('O dobro de', n1, 'é {} e o triplo é {}, já sua raiz quadrada é {}'.format(n1*2, n1*3, n1**(1/2)))
+n = int(input('Digite um número: '))
+print('O dobro de', n, 'é {} e o triplo é {}, já sua raiz quadrada é {}'.format(n*2, n*3, n**(1/2)))
