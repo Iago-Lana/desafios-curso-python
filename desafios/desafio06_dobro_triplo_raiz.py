81e65dfdb6187c62a72902869620b169be9746f4
Iago De Lana <iagoferlana@gmail.com>
2025-10-17 16:57:39 -0300

    Adiciona o desafio 06 de dobro, triplo e raiz.

---
+++ desafios/desafio06_dobro_triplo_raiz.py
@@ -0,0 +1,6 @@
+"""
+Desafio 06: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+"""
+n1 = int(input('Digite um número: '))
+print('O dobro de', n1, 'é {} e o triplo é {}, já sua raiz quadrada é {}'.format(n1*2, n1*3, n1**(1/2)))
