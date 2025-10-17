9bc3d93d3890bafa4b24e63e45a9b6d3eb555825
Iago De Lana <iagoferlana@gmail.com>
2025-10-17 17:44:00 -0300

    Adiciona desafio 07 de média do aluno

---
+++ desafios/desafio07_media_aluno.py
@@ -0,0 +1,7 @@
+"""
+Desafio 07: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+"""
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+print('Primeira nota:', n1, '\nSegunda nota:', n2, '\nSendo assim a média do aluno foi {}'.format((n1+n2)/2))
