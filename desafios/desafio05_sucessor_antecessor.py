0fd1fe5c30034685ef8cb4e5ad3daf822425cb3a
Iago De Lana <iagoferlana@gmail.com>
2025-10-17 16:33:54 -0300

    Adiciona o desafio 05 de sucessor e antecessor.

---
+++ desafios/desafio05_sucessor_antecessor.py
@@ -0,0 +1,6 @@
+"""
+Desafio 05: Faça um progrma que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+"""
+n1 = int(input('Digite um número inteiro: '))
+print('O sucessor de {} é {} e o antecessor é {}'.format(n1, n1+1, n1-1))
