19750d3ad940e2226d99419502c005d959db4441
Iago De Lana <iagoferlana@gmail.com>
2025-10-20 21:10:35 -0300

    Adiciona o desafio 014 de conversor de temperatura.

---
+++ desafios/desafio014_conversor_de_temperatura.py
@@ -0,0 +1,6 @@
+"""
+Desafio 014: Faça um programa que converta a temperatura digitada em °C e converta para °F
+
+"""
+C = float(input('Digite a temperatura em C°:'))
+print('A temperatura de {}°C corresponde a {}°F'.format(C, (C*9/5)+32))
