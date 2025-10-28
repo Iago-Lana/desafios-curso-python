a1098fda467957d1fedeac95efd3d00aada2750d
Iago De Lana <iagoferlana@gmail.com>
2025-10-27 22:43:00 -0300

    Adiciona o desafio 021 de MP3

---
+++ desafios/desafio021_mp3.py
@@ -0,0 +1,6 @@
+"""
+Desafio 021: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3
+
+"""
+from playsound import playsound
+playsound(r'C:\Users\angel\Downloads\make the visible invisible - Alge.mp3')
