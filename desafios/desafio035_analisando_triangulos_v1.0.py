729436ccbc9003e852cb65c6b90cee86e96664fc
Iago De Lana <iagoferlana@gmail.com>
2025-11-10 20:32:00 -0300

    Adiciona o desafio 035 de analisando triângulos v1.0

---
+++ desafios/desafio035_analisando_triangulos_v1.0.py
@@ -0,0 +1,13 @@
+"""
+Desafio 035: Desenvolva um programa que leia o comprimento de
+três retas e diga ao usuário se elas podem ou não
+formar um triângulo.
+"""
+r1 = float(input('Digite a primeira reta: '))
+r2 = float(input('Digite a segunda reta: '))
+r3 = float(input('Digite a terceira reta: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima podem formar um triângulo!')
+else:
+    print('Os segmentos acima não podem formar um triângulo.')
+ 
