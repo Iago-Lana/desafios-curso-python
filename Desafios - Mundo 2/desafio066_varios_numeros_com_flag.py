a2180e7edefc862c2546a7a256417e56093b310b
Iago De Lana <iagoferlana@gmail.com>
2026-02-22 21:35:41 -0300

    Adiciona o desafio 066 de Vários números com flag.

---
+++ Desafios - Mundo 2/desafio066_varios_numeros_com_flag.py
@@ -0,0 +1,25 @@
+"""
+Desafio 066: Crie um programa
+que leia vários
+números inteiros pelo
+teclado. O programa só
+vai parar quando o
+usuário digitar o valor
+999, que é a condição
+de parada. No final,
+mostre quantos
+números foram
+digitados e qual foi a
+soma entre eles.
+(desconsiderando o flag)
+"""
+s = 0
+cont = 0
+while True:
+    n = int(input('Digite um número inteiro: '))
+    if n == 999:
+        break
+    s += n
+    cont += 1
+print(f'Soma dos números digitados: {s}')
+print(f'Total de números digitados: {cont}')
