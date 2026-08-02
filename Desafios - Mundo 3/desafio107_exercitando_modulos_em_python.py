cf0f3f2b95b0b92ba2f682fe13bea7624c7cd98c
Iago De Lana <iagoferlana@gmail.com>
2026-08-01 23:26:04 -0300

    Adiciona o desafio 107 de Exercitando Módulos em Python e o Módulo moeda.py

---
+++ Desafios - Mundo 3/desafio107_exercitando_modulos_em_python.py
@@ -0,0 +1,21 @@
+"""
+Desafio 107: Crie um módulo
+chamado moeda.py que
+tenha as funções
+incorporadas
+aumentar(), diminuir(),
+dobro() e metade().
+
+Faça também um
+programa que importe
+esse modulo e use
+algumas dessas
+funções.
+"""
+import moeda
+
+p = float(input('Digite o preço: R$'))
+print(f'A metade de R${p:.2f} é R${moeda.metade(p):.2f}')
+print(f'O Dobro de R${p:.2f} é R${moeda.dobro(p):.2f}')
+print(f'Aumentando 10%, temos R${moeda.aumentar(p, 10):.2f}')
+print(f'Reduzindo 13%, temos R${moeda.diminuir(p, 13):.2f}')
---
+++ Desafios - Mundo 3/moeda.py
@@ -0,0 +1,16 @@
+def aumentar(n, p):
+    p = p / 100 * n
+    return n + p
+
+
+def diminuir(n, p):
+    p = p / 100 * n
+    return n - p
+
+
+def dobro(n):
+    return n * 2
+
+
+def metade(n):
+    return n / 2
