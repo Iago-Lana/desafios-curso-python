3936157ab3ffb2d9a693d28475f015e53c49192a
Iago De Lana <iagoferlana@gmail.com>
2026-08-07 20:54:57 -0300

    Adiciona o desafio 110 de Reduzindo ainda mais seu programa e afunção resumo() no Módulo moeda.py

--- Desafios - Mundo 3/moeda.py
+++ Desafios - Mundo 3/moeda.py
@@ -27,3 +27,15 @@ def metade(n, f=False):
 def moeda(n):
     n = f'R${n:.2f}'.replace('.',',')
     return n
+
+
+def resumo(n, a, d):
+    print('='*35)
+    print(f'{'RESUMO DO VALOR':>24}')
+    print('='*35)
+    print(f'Preço analisado: \t{moeda(n)}')
+    print(f'Dobro do preço: \t{dobro(n, True)}')
+    print(f'Metade do preço: \t{metade(n, True)}')
+    print(f'{a}% de aumento: \t{aumentar(n,a ,True)}')
+    print(f'{d}% de redução: \t{diminuir(n,d ,True)}')
+    print('='*35)
